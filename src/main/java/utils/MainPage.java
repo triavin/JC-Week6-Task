e57513ad58499314f6900a488d4402c8cb06eab7
Aldi Triavin <aldi3avin@gmail.com>
2023-10-29 16:22:46 +0700

    create utils

---
+++ src/main/java/utils/MainPage.java
@@ -0,0 +1,11 @@
+package utils;
+
+public class MainPage {
+    public static final String T1_1 = "202112SOAL003";
+    public static final String T1_2 = "202111SOAL03";
+    public static final String T1_3 = "202110SOAL03";
+
+    public static final String T2_1 = "SOAL3/2112/AB000000011 OD:0001234500CDE5432100 SOAL003 ABCDE12345 XAS SKILL TEST ESSAY .DT";
+    public static final String T2_2 = "SOAL3/2111/BC000011100 OD:0003452100EFG2451300 SOAL003 EFGHI25134 XAS SKILL TEST ESSAY .DT";
+    public static final String T2_3 = "SOAL3/2110/DE210031010 OD:0001524300HIJ2145300 SOAL003 JKLMN52431 XAS SKILL TEST ESSAY .DT";
+}
